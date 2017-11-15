42483f9dfd9aa48c48a310ed32932d11105e49da
nick <nick@NICK-PC>
2017-11-15 10:58:31 -0700

    Add project files.

---
+++ WilliamsWeb1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WilliamsWeb1.MvcApplication" Language="C#" %>
---
+++ WilliamsWeb1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WilliamsWeb1.Startup))]
+namespace WilliamsWeb1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
